e25709d07d827acc79038854ae772b1c3fa02123
Jacovia Cherry <jcherry@clemson.edu>
2019-10-09 19:51:53 -0400

    queue pointer type changed

--- ece223/hw3.c/queue.h
+++ ece223/hw3.c/queue.h
@@ -4,19 +4,19 @@
 #define Q_H
 
 /** list data pointer. not to be dereferenced or freed by the list. */
-//typedef void *qdata;
 typedef int qdata;
 /* pointer to opaque queue type*/
-typedef struct queue queue;
-typedef queue *q; 	//queue pointer
+//typedef struct queue queue;
+typedef void *q; 	//queue pointer
 
 /*function declarations*/
 void initialize(*q);
-front();
+/*
+void front();
 void isempty();
-isfull();
+void isfull();
 void enqueue(q, qdata data);
 qdata dequeue(q);
-destroy(*q);
-
+void destroy(*q);
+*/
 #endif Q_H
