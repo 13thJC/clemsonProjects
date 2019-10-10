fa8cb3ac88746c1872d00f273e1ee1d637cfd1aa
Jacovia Cherry <jcherry@clemson.edu>
2019-10-10 15:03:02 -0400

    fixed incompatible data types for initialize and isempty functions

--- ece223/hw3.c/queue.h
+++ ece223/hw3.c/queue.h
@@ -6,17 +6,16 @@
 /** list data pointer. not to be dereferenced or freed by the list. */
 typedef int qdata;
 /* pointer to opaque queue type*/
-//typedef struct queue queue;
-typedef void *q; 	//queue pointer
+typedef	struct Queue queue;
 
 /*function declarations*/
-void initialize(*q);
+queue* initialize();
+int isempty(queue *q);
 /*
 void front();
-void isempty();
 void isfull();
 void enqueue(q, qdata data);
 qdata dequeue(q);
 void destroy(*q);
 */
-#endif Q_H
+#endif
